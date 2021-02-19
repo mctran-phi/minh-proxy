5b6c8f06e53c4f7aae072e08d4c0e767d14c9dff
Minh Tran <mctran98@yahoo.com>
2021-02-18 18:43:59 -0800

    Changed port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 app = express();
-const port = 8080;
+const port = 3333;
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
