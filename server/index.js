b538a2cf52b62433fb805f2433b6b7f59bab008a
Minh Tran <mctran98@yahoo.com>
2021-08-23 17:15:35 -0700

    Removed images and changed port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 app = express();
-const port = 3333;
+const port = 7000;
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
